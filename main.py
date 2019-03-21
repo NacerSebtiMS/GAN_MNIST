f0e6f4693fd977f948ac2704c24f69b01b0f4985
Nacer Sebti <sebn3001@codage.local>
2019-03-21 16:38:10 -0400

    Fixes + added a testing function

--- main.py
+++ main.py
@@ -12,18 +12,40 @@ train_image_path = 'data/train-images-idx3-ubyte.gz'
 train_label_path = 'data/train-labels-idx1-ubyte.gz'
 
 
-def label(file,n):
-	f.read(8)
-	buff = f.read(1 * n)
-	labels = np.frombuffer(buff, dtype=np.uint8).astype(np.int64)
-	return labels[-1]
+def get_label(n,train=True):
+    if train:
+        file = gzip.open(train_label_path,'r')
+    else :
+        file = gzip.open(test_label_path,'r')
+        
+    file.read(8)
+    buff = file.read(1 * n)
+    labels = np.frombuffer(buff, dtype=np.uint8).astype(np.int64)
+    return labels[-1]
 
 
-def img(file,n):
-	image_size = 28
-	f.read(16)
-	buff = f.read(image_size * image_size * n)
-	data = np.frombuffer(buff, dtype=np.uint8).astype(np.float32)
-	data = data.reshape(n, image_size, image_size, 1)
-	image = np.asarray(data[-1]).squeeze()
-	return image
+def get_img(n,train=True):
+    if train:
+        file = gzip.open(train_image_path,'r')
+    else :
+        file = gzip.open(test_image_path,'r')
+        
+    image_size = 28
+    file.read(16)
+    buff = file.read(image_size * image_size * n)
+    data = np.frombuffer(buff, dtype=np.uint8).astype(np.float32)
+    data = data.reshape(n, image_size, image_size, 1)
+    image = np.asarray(data[-1]).squeeze()
+    return image
+
+def get_both(n,train=True):
+    return get_label(n,train),get_img(n,train)
+
+def test():
+    n = 1500
+    lbl,img = get_both(n)
+    print(lbl)
+    plt.imshow(img)
+
+if __name__ == "__main__":
+    test()
