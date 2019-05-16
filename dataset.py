0c4c7f34af35660faf101a2997c3269bb9f051eb
Nacer Sebti <sebn3001@codage.local>
2019-05-16 18:00:59 -0400

    Added helper functions to dataset

--- dataset.py
+++ dataset.py
@@ -12,6 +12,7 @@ train_image_path = 'data/train-images-idx3-ubyte.gz'
 train_label_path = 'data/train-labels-idx1-ubyte.gz'
 
 
+
 def get_label(n,train=True):
     if train:
         file = gzip.open(train_label_path,'r')
@@ -41,11 +42,44 @@ def get_img(n,train=True):
 def get_both(n,train=True):
     return get_label(n,train),get_img(n,train)
 
-def test():
-    n = 1500
-    lbl,img = get_both(n)
-    print(lbl)
+def get_img_by_lbl(n,label,train=True):
+    cpt=1
+    imgs = []
+    while n>len(imgs):
+        if get_label(cpt,train) == label:
+            imgs += [get_img(cpt,train)]
+        cpt+=1
+    return imgs
+
+#----------------------------------------------------------------------------
+# Functions used to print the results
+
+def show_img(img):
     plt.imshow(img)
+
+def show_mult_img(imgs):
+    n = len(imgs)
+    l = int(n**(1/2))
+    if int(n**(1/2)) != n**(1/2):
+        l+=1
+    fig = plt.figure(figsize=(l*2,l*2))
+    for i in range(len(imgs)):
+        sub = fig.add_subplot(l,l,i+1)
+        sub.imshow(imgs[i], interpolation='nearest')
+
+#----------------------------------------------------------------------------
+# Testing Area
+        
+def test():
+#    n = 15
+#    lbl,img = get_both(n)
+#    print(lbl)
+#    plt.imshow(img)
+    n = 16
+    label = 4
+    imgs = get_img_by_lbl(n,label)
+    i=0
+    show_mult_img(imgs)
 
 if __name__ == "__main__":
     test()
